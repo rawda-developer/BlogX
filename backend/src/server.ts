0e325ac5f30c96f04a1fceaa61ab8dac906d4c10
Rawda Yasser <rawda.developer@gmail.com>
2023-02-20 12:51:21 +0200

    Feat: add auth routes to the api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
-import express, { Express, Request, Response } from 'express';
-
-const app:Express = express();
-app.get("/", (req:Request, res:Response) => {
-    res.json({message: "BlogX backend"})
-}) 
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import userRoutes from "./routes/users.route";
+import authRoutes from "./routes/auth.route";
+const app: Express = express();
+app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "BlogX backend" });
+});
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 app.listen(4000, () => console.log("Listening on port 4000!!"));
+export default app;
