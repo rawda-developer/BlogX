e0f8bc43e0555cf45da309eaa1637146f5742510
Rawda Yasser <rawda.developer@gmail.com>
2023-02-21 13:24:22 +0200

    Refactor: extract seed users and seed articles to seperate functions

--- utils/seedFn.ts
+++ utils/seedFn.ts
@@ -1,16 +1,36 @@
 import User from "../models/user.model";
+import Article from "../models/article.model";
 const name = "Test 1";
 const password = "a_password";
-
 const email = "test_user@example.org";
 // const validAuth = Buffer.from(`${username}:${password}`).toString("base64");
 // const authHeader = `Basic ${validAuth}`;
 
 let user;
-const seedData = async () => {
+const seedArticles = async () => {
+  const article1 = new Article({
+    title: "Test Article 1 title",
+    text: "Hello world Article 1",
+  });
+  await article1.save();
+  const article2 = new Article({
+    title: "Test Article 2 title",
+    text: "Hello world Article 2",
+  });
+  await article2.save();
+  console.log(await Article.find({}));
+};
+const seedUsers = async () => {
   user = new User({
-    name, email, password
+    name,
+    email,
+    password,
   });
   await user.save();
+  console.log(await User.find({}));
+};
+const seedData = async () => {
+  await seedUsers();
+  await seedArticles();
 };
 export { seedData, user };
